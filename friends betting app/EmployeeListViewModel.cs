feef076ce47ecf224d16f9cd888f02af0cf7dd92
Ben Johnson <jimbixeren@gmail.com>
2021-07-29 23:30:53 +0200

    Add project files.

---
+++ friends betting app/EmployeeListViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace friends_betting_app
+{
+    class EmployeeListViewModel
+    {
+        public ICommand AddEmployeeCommand => new Command(AddEmployee);
+        public ICommand RemoveEmployeeCommand => new Command(RemoveEmployee);
+        public ObservableCollection<string> Employees { get; set; }
+        public string EmployeeName { get; set; }
+        public string SelectedEmployee { get; set; }
+        
+
+        public EmployeeListViewModel()
+        {
+            //string[] arrEmployees = new string[] { "Rob Finnerty", "Bill Wrestler", "Gert-Beth Hopper", "Keith joyce-Purdy", "Sheri Spruce", "Burt Indybrick" };
+            //Employees = arrEmployees;
+
+            Employees = new ObservableCollection<string>();
+
+            Employees.Add("Rob Finnerty");
+            Employees.Add("Bill Wrestler");
+            Employees.Add("Gert-Beth Hopper");
+            Employees.Add("Keith joyce-Purdy");
+            Employees.Add("Sheri Spruce");
+            Employees.Add("Burt Indybrick");
+
+
+
+        }
+        public void AddEmployee()
+        {
+            Employees.Add(EmployeeName);
+
+        }
+        public void RemoveEmployee()
+        {
+
+            Employees.Remove(SelectedEmployee);
+        }
+
+    }
+}
---
+++ friends betting app/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace friends_betting_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+           // lvEmployees.ItemsSource = new string[] { "Rob Finnerty", "Bill Wrestler", "Gert-Beth Hopper", "Keith joyce-Purdy", "Sheri Spruce", "Burt Indybrick" };
+        }
+    }
+}
